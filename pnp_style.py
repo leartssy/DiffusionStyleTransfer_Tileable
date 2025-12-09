0d275180c6b6e9609b4489846b8988630b5ac769
stleeschl <lea.eschlberger@stud.h-da.de>
2025-12-09 16:05:10 +0100

    textile 0.1

--- pnp_style.py
+++ pnp_style.py
@@ -311,7 +311,7 @@ class BLIP_With_Textile(BlipDiffusionPipeline):
         style_start_index = int(self._ddim_steps * self._alpha)
         style_stop_index = int(num_inference_steps - 9)
         #textile start -> delay running of textile into last steps
-        tex_start = 0.0
+        tex_start = 0.1
         Textile_start_step = int(num_inference_steps * tex_start)
         
 
