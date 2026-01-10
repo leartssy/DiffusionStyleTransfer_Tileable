5827668cfab3e890d1e91d411c8cf78d2940397a
stleeschl <lea.eschlberger@stud.h-da.de>
2026-01-10 14:36:51 +0100

    fix?

--- pnp_utils_style.py
+++ pnp_utils_style.py
@@ -117,8 +117,8 @@ def register_attention_control_efficient(model, injection_schedule, attention_we
                     
                 q = self.to_q(x)
                 k = self.to_k(encoder_hidden_states)
-                v = self.to_v(encoder_hidden_states)
-                source_batch_size = int(q.shape[0] // 3)
+                
+                source_batch_size = int(q.shape[0] // 2)
 
                 # blended attention injection
 
@@ -135,6 +135,7 @@ def register_attention_control_efficient(model, injection_schedule, attention_we
 
                 q = self.head_to_batch_dim(q)
                 k = self.head_to_batch_dim(k)
+                v = self.to_v(encoder_hidden_states)
                 v = self.head_to_batch_dim(v)
 
 
@@ -142,9 +143,18 @@ def register_attention_control_efficient(model, injection_schedule, attention_we
                 q = self.to_q(x)
                 k = self.to_k(encoder_hidden_states)
                 v = self.to_v(encoder_hidden_states)
+                w = 0.8
+                source_batch_size = int(q.shape[0] // 3)
+                #第一部分content第二部分无条件的第三部分有条件的
+                # inject unconditional
+                k[source_batch_size:2 * source_batch_size] = k[:source_batch_size]
+                v[source_batch_size:2 * source_batch_size] = v[:source_batch_size]
                 
-                # No injection logic 
                 
+                # inject conditional
+                k[2 * source_batch_size:] = k[:source_batch_size]
+                v[2 * source_batch_size:] = v[:source_batch_size]
+
                 q = self.head_to_batch_dim(q)
                 k = self.head_to_batch_dim(k)
                 v = self.head_to_batch_dim(v)
