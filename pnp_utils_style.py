6d42048a78edc6e756f4ee3ebd4112aefca5f7dc
stleeschl <lea.eschlberger@stud.h-da.de>
2026-01-10 14:05:57 +0100

    attention 0 enabled

--- pnp_utils_style.py
+++ pnp_utils_style.py
@@ -110,47 +110,48 @@ def register_attention_control_efficient(model, injection_schedule, attention_we
 
             is_cross = encoder_hidden_states is not None
             encoder_hidden_states = encoder_hidden_states if is_cross else x
-            if not is_cross and self.injection_schedule is not None and (
-                    self.t in self.injection_schedule or self.t == 1000):
-                q = self.to_q(x)
-                k = self.to_k(encoder_hidden_states)
-                v = self.to_v(encoder_hidden_states)
+            if not is_cross and self.injection_schedule is not None and attention_weight > 0:
+                if self.t in self.injection_schedule or self.t == 1000:
+                    q = self.to_q(x)
+                    k = self.to_k(encoder_hidden_states)
+                    v = self.to_v(encoder_hidden_states)
 
-                source_batch_size = int(q.shape[0] // 3)
+                    source_batch_size = int(q.shape[0] // 3)
 
-                # blended attention injection
+                    # blended attention injection
 
-                # inject unconditional
-                #q[source_batch_size:2 * source_batch_size] = q[:source_batch_size] 
-                #k[source_batch_size:2 * source_batch_size] = k[:source_batch_size] 
-                k[source_batch_size:2 * source_batch_size] = (1 - attention_weight) * k[source_batch_size:2 * source_batch_size] + attention_weight * k[:source_batch_size]
-                v[source_batch_size:2 * source_batch_size] = (1 - attention_weight) * v[source_batch_size:2 * source_batch_size] + attention_weight * v[:source_batch_size]
-                # inject conditional
-                k[2 * source_batch_size:] = (1 - attention_weight) * k[2 * source_batch_size:] + attention_weight * k[:source_batch_size]
-                v[2 * source_batch_size:] = (1 - attention_weight) * v[2 * source_batch_size:] + attention_weight * v[:source_batch_size]
-                #q[2 * source_batch_size:] = q[:source_batch_size] 
-                #k[2 * source_batch_size:] = k[:source_batch_size]
+                    # inject unconditional
+                    #q[source_batch_size:2 * source_batch_size] = q[:source_batch_size] 
+                    #k[source_batch_size:2 * source_batch_size] = k[:source_batch_size] 
+                    k[source_batch_size:2 * source_batch_size] = (1 - attention_weight) * k[source_batch_size:2 * source_batch_size] + attention_weight * k[:source_batch_size]
+                    v[source_batch_size:2 * source_batch_size] = (1 - attention_weight) * v[source_batch_size:2 * source_batch_size] + attention_weight * v[:source_batch_size]
+                    # inject conditional
+                    k[2 * source_batch_size:] = (1 - attention_weight) * k[2 * source_batch_size:] + attention_weight * k[:source_batch_size]
+                    v[2 * source_batch_size:] = (1 - attention_weight) * v[2 * source_batch_size:] + attention_weight * v[:source_batch_size]
+                    #q[2 * source_batch_size:] = q[:source_batch_size] 
+                    #k[2 * source_batch_size:] = k[:source_batch_size]
 
-                q = self.head_to_batch_dim(q)
-                k = self.head_to_batch_dim(k)
-                v = self.head_to_batch_dim(v)
+                    q = self.head_to_batch_dim(q)
+                    k = self.head_to_batch_dim(k)
+                    v = self.head_to_batch_dim(v)
 
 
             else :
                 q = self.to_q(x)
                 k = self.to_k(x)
                 v = self.to_v(x)
-                w = 0.8
-                source_batch_size = int(q.shape[0] // 3)
-                #第一部分content第二部分无条件的第三部分有条件的
-                # inject unconditional
-                k[source_batch_size:2 * source_batch_size] = k[:source_batch_size]
-                v[source_batch_size:2 * source_batch_size] = v[:source_batch_size]
                 
-                
-                # inject conditional
-                k[2 * source_batch_size:] = k[:source_batch_size]
-                v[2 * source_batch_size:] = v[:source_batch_size]
+                if attention_weight > 0:
+                    source_batch_size = int(q.shape[0] // 3)
+                    #第一部分content第二部分无条件的第三部分有条件的
+                    # inject unconditional
+                    k[source_batch_size:2 * source_batch_size] = k[:source_batch_size]
+                    v[source_batch_size:2 * source_batch_size] = v[:source_batch_size]
+                    
+                    
+                    # inject conditional
+                    k[2 * source_batch_size:] = k[:source_batch_size]
+                    v[2 * source_batch_size:] = v[:source_batch_size]
 
                 q = self.head_to_batch_dim(q)
                 k = self.head_to_batch_dim(k)
